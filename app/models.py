37b1efd353eac592a03419d7b973f8617204bf86
JC5LZiy3HVfV5ux <ejow-artem@yandex.ru>
2021-12-03 21:42:32 +0700

    delete generate_auth_token func in user model

--- app/models.py
+++ app/models.py
@@ -208,20 +208,6 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
-    def generate_auth_token(self, expiration):
-        s = Serializer(current_app.config['SECRET_KEY'],
-                       expires_in=expiration)
-        return s.dumps({'id': self.id}).decode('utf-8')
-
-    @staticmethod
-    def verify_auth_token(token):
-        s = Serializer(current_app.config['SECRET_KEY'])
-        try:
-            data = s.loads(token)
-        except:
-            return None
-        return User.query.get(data['id'])
-
     def can(self, perm):
         return self.role is not None and self.role.has_permission(perm)
 
